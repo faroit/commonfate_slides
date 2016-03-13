d077c59c1410c99bc4eec09481bbe833578318b9
Fabian-Robert Stöter <mail@faroit.com>
2016-03-14 00:10:18 +0100

    adding audio file and patch renderer

--- data/render_patches.py
+++ data/render_patches.py
@@ -1,58 +1,17 @@
-import modules.mstft as mstft
+import commonfate
 import pylab as plt
 import numpy as np
+np.random.seed(64)
 import itertools
 import soundfile as sf
 import argparse
 import yaml
 import seaborn as sns
 import matplotlib as mpl
-
-
-"""-------------------------------------------------------------------------------------------------
-Modulation NMF, model with
-
-each source modulation spectrogram Vj(a,b,f,t) modeled as
-Vj(a,b,f,t,c) = P(a,b,f,j)At(t,j)Ac(c,j)
-
-So we have one modulation "shape" for each frequency, hence P(a,b,f,j)
-which is activated over time, this is At(t,j)  and over channels, this is
-Ac(c,j)
-
--------------------------------------------------------------------------------------------------
-Antoine Liutkus, Inria 2014"""
+import scipy.signal
 
 
 def displaySTFT(X, name=None):
-    # display a modulation spectrogram, of shape (w1,w2,f,t)
-    plt.rc('text', usetex=True)
-    mpl.rcParams['font.family'] = 'serif'
-    mpl.rcParams['text.latex.unicode'] = 'True'
-
-    sns.set()
-    sns.set_context("paper")
-    sns.set_style(
-        "white", {
-            "grid.color": '.9',
-            "font.family":
-            "serif", 'font.serif':
-            'ptmrr8re'
-        }
-    )
-
-    params = {'backend': 'ps',
-              'axes.labelsize': 14,
-              'font.size': 14,
-              'legend.fontsize': 12,
-              'xtick.labelsize': 12,
-              'ytick.labelsize': 12,
-              'text.usetex': True,
-              'font.family': 'serif',
-              'font.serif': 'ptmrr8re',
-              }
-
-    plt.rcParams.update(params)
-
     fig, ax = plt.subplots(1, 1)
     plt.figure(1)
     plt.pcolormesh(
@@ -61,9 +20,13 @@ def displaySTFT(X, name=None):
         vmax=10,
         cmap='cubehelix_r',
     )
-    plt.axis((0, 240, 0, 352))
-    fig.tight_layout()
-    fig.savefig(name, bbox_inches='tight', dpi=300)
+    # plt.axis((0, 240, 0, 352))
+    # fig.tight_layout()
+
+    if name is None:
+        plt.show()
+    else:
+        plt.savefig(name)
 
 
 def displayMSTFT(Z, name=None):
@@ -78,7 +41,7 @@ def displayMSTFT(Z, name=None):
             vmax=10,
             cmap='cubehelix_r',
         )
-        plt.axis((0, 48, 4, 28))
+        # plt.axis((0, 48, 4, 28))
         plt.xticks([])
         plt.xlabel('')
         plt.yticks([])
@@ -102,23 +65,12 @@ def process(
         display=True,
         save=True
 ):
-    '''Applies CFM Separation
 
-    Args:
-       signal (ndarray):    Single or multichannel audio signal
-       rate (float):        sample rate
-       pref (object):       Preference object
-
-    Returns:
-       output (ndarray):    output array separated into pref.k components or
-                            clustered into pref.s components using kmeans
-
-    '''
     W = (pref['W_A'], pref['W_B'])
     mhop = (pref['mhop_A'], pref['mhop_B'])
 
     print 'computing STFT'
-    xstft = mstft.stft(xwave, pref['nfft'], pref['thop'])
+    xstft = commonfate.transform.forward(xwave, pref['nfft'], pref['thop'])
     print xstft.shape
 
     xstft = xstft[:384-32, 16:256]
@@ -126,23 +78,58 @@ def process(
 
     # compute modulation STFT
     print 'computing modulation STFT'
-    x = mstft.stft(xstft, W, mhop, real=False)
-    # m = mstft.stft(np.abs(xstft), W, mhop, real=False)
-    g = mstft.split(xstft, W, mhop, False, verbose)
+    x = commonfate.transform.forward(xstft, W, mhop, real=False)
 
     print 'getting modulation spectrogram, shape:', x.shape
     z = np.abs(x) ** pref['alpha']
 
-    if save:
-        np.savez_compressed('gridstft.npz', g[..., :, :, 0])
-        np.savez_compressed('cft.npz', x[..., :, :, 0])
+    # initialiase and fit the common fate model
+    cfm = commonfate.model.CFM(z, nb_iter=100, nb_components=pref['J']).fit()
+
+    # get the fitted factors
+    (P, At, Ac) = cfm.factors
+
+    # returns the of z approximation using the fitted factors
+    z_hat = cfm.approx
+
+    # source estimates
+    estimates_k = []
+    z_k = []
+    stf_k = []
+
+    for j in range(pref['J']):
+
+        Fj = commonfate.model.hat(
+            P[..., j][..., None],
+            At[..., j][..., None],
+            Ac[..., j][..., None],
+        )
+
+        yj = Fj / z_hat * x
+        z_k.append(yj)
+        # first compute back STFT
+        yjstft = commonfate.transform.inverse(
+            yj, fdim=2, hop=mhop, shape=xstft.shape, real=False
+        )
+        stf_k.append(yjstft)
+        # then waveform
+        wavej = commonfate.transform.inverse(
+            yjstft, fdim=1, hop=pref['thop'], shape=signal.shape
+        )
+        estimates_k.append(wavej)
+    #
+    displaySTFT(At)
+    displayMSTFT(P)
 
     if display:
-        displaySTFT(xstft, 'out2.pdf')
-        displayMSTFT(g[..., :, :, 0], 'out1.png')
-        # displayMSTFT(m[..., :, :, 0], 'out2.pdf')
-        displayMSTFT(z[..., :, :, 0], 'out3.png')
-
+        displaySTFT(xstft, 'stft.png')
+        displaySTFT(stf_k[0], 'stft_k0.png')
+        displaySTFT(stf_k[1], 'stft_k1.png')
+        displayMSTFT(z[..., 0], 'cft.png')
+        displayMSTFT(z_k[0][..., 0], 'cft_k0.png')
+        displayMSTFT(z_k[1][..., 0], 'cft_k1.png')
+        displaySTFT(At, 'At.png')
+        displayMSTFT(P, 'P.png')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -161,6 +148,7 @@ if __name__ == '__main__':
     filename = args.input
 
     # loading signal
-    (xwave, fs) = sf.read(filename)
-
+    (xwave, fs) = sf.read(filename, always_2d=True)
+    # xwave = scipy.signal.decimate(xwave, 2)
+    # fs = fs/2
     out = process(xwave, fs, pref)
